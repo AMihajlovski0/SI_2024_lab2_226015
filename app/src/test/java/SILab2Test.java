9eb20b217d0fb768778928ab147233738f8d3e63
AMihajlovski0 <adrian.mihajlovski07@gmail.com>
2024-05-26 15:36:00 +0200

    Statically imported Assertions and fixed order of assertEquals arguments

--- app/src/test/java/SILab2Test.java
+++ app/src/test/java/SILab2Test.java
@@ -1,4 +1,4 @@
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import java.util.Collections;
@@ -7,38 +7,38 @@ public class SILab2Test {
     @Test
     public void testAllBranches() {
         {
-            RuntimeException e = Assertions.assertThrows(
+            RuntimeException e = assertThrows(
                     RuntimeException.class,
                     () -> SILab2.checkCart(null, 1)
             );
-            Assertions.assertEquals(e.getMessage(), "allItems list can't be null!");
+            assertEquals("allItems list can't be null!",e.getMessage());
         }
 
         {
-            RuntimeException e = Assertions.assertThrows(
+            RuntimeException e = assertThrows(
                     RuntimeException.class,
                     () -> SILab2.checkCart(Collections.singletonList(new Item(null,null,0,0)),1)
             );
-            Assertions.assertEquals(e.getMessage(), "No barcode!");
+            assertEquals("No barcode!",e.getMessage());
         }
 
         {
-            RuntimeException e = Assertions.assertThrows(
+            RuntimeException e = assertThrows(
                     RuntimeException.class,
                     () -> SILab2.checkCart(Collections.singletonList(new Item("A","a",0,0)),1)
             );
-            Assertions.assertEquals(e.getMessage(), "Invalid character in item barcode!");
+            assertEquals("Invalid character in item barcode!",e.getMessage());
         }
 
-        Assertions.assertTrue(SILab2.checkCart(Collections.singletonList(new Item("A", "0", 0, 0)), 1));
-        Assertions.assertFalse(SILab2.checkCart(Collections.singletonList(new Item("A", "0", 301, 1)), 1));
+        assertTrue(SILab2.checkCart(Collections.singletonList(new Item("A", "0", 0, 0)), 1));
+        assertFalse(SILab2.checkCart(Collections.singletonList(new Item("A", "0", 301, 1)), 1));
     }
 
     @Test
     public void testMultipleConditions() {
-        Assertions.assertFalse(SILab2.checkCart(Collections.singletonList(new Item("A", "1", 300, 0)), 250));
-        Assertions.assertFalse(SILab2.checkCart(Collections.singletonList(new Item("A", "1", 301, 0)), 250));
-        Assertions.assertFalse(SILab2.checkCart(Collections.singletonList(new Item("A", "1", 301, 0.9f)), 250));
-        Assertions.assertTrue(SILab2.checkCart(Collections.singletonList(new Item("A", "0", 301, 0.9f)), 250));
+        assertFalse(SILab2.checkCart(Collections.singletonList(new Item("A", "1", 300, 0)), 250));
+        assertFalse(SILab2.checkCart(Collections.singletonList(new Item("A", "1", 301, 0)), 250));
+        assertFalse(SILab2.checkCart(Collections.singletonList(new Item("A", "1", 301, 0.9f)), 250));
+        assertTrue(SILab2.checkCart(Collections.singletonList(new Item("A", "0", 301, 0.9f)), 250));
     }
 }
